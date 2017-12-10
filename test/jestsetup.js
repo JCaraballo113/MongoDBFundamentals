bd5f9ef312a59e77a9d1178a499e580a2acb0155
John <jcaraballo019@gmail.com>
2017-12-10 00:25:32 -0400

    Removed jest

--- test/jestsetup.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true });
-mongoose.connection
-.once('open', () => {})
-.on('error', (error) => console.warn('Warning', error));
